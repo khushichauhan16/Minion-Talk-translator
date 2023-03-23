19e938c70ffac66ec4fe7d8f6346380bb46fbd91
khushichauhan16 <128140878+khushichauhan16@users.noreply.github.com>
2023-03-23 19:45:27 +0530

    docs: all details added

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-var translatorUrl="https://api.funtranslations.com/translate/ferb-latin.json";
+var translatorUrl="https://api.funtranslations.com/translate/minion.json";
+var input=document.querySelector(".inputArea");
+var output=document.querySelector(".outputArea");
+var translator=document.querySelector(".button");
+function gettranslatorUrl(input)
+{
+    return translatorUrl+"?"+"text="+input;
+}
+function buttonclick()
+{
+    var textInput=input.value;
+    fetch(gettranslatorUrl(textInput))
+    .then(response=>response.json())
+    .then(json=>
+        {
+        var translatedText=json.contents.translated;
+        output.innerText=translatedText;
+        })
+}
+translator.addEventListener("click",buttonclick);
